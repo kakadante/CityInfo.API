ad1c208a873acb85999ade0f3ad6b3c2f771d873
kaka_dante <danielmasila76@gmail.com>
2020-02-01 15:05:04 +0300

    START WITH CREATING A CONTROLLER
    ~ CitiesController
    
    then above--input
            [HttpGet("api/cities")]

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+
+        public JsonResult GetCities()
+        {
+                return new JsonResult(new List<object>()
+                {
+                    new {id =1, Name="New York City"},
+                    new {id =2, Name="Antwerp"}
+                });
+        }
+    }
+}
